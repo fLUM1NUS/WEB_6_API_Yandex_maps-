b241b9b18a1d83c24dbe8db010d93b020d2ffd71
vadicgames <vdcgms@mail.ru>
2023-02-06 15:08:51 +0900

    che'?

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class Window(QMainWindow):
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
+
 if __name__ == '__main__':
     sys.excepthook = except_hook
     app = QApplication(sys.argv)
